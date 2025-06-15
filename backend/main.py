b593e41ce32d9db15299b55113a7c17af0fc056f
TANAY895 <tanaymaniyar895@gmail.com>
2025-06-15 22:54:25 +0530

    Fix import path for correct_code

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
